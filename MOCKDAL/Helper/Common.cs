d2a94b2cfcb71a8136a29e88e726c41ea6a38086
remyau <remya.sudhakaran@gmail.com>
2023-09-11 22:33:59 -0400

    Add project files.

---
+++ MOCKDAL/Helper/Common.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCKDAL.Helper
+{
+    public class Common
+    {
+        public static List<UI.MOCKTBL> GetEntries()
+        {
+            using (var db = new MOCKTESTDBEntities())
+            {
+                var cust = from tbl in db.MOCKTBLs
+                           select new UI.MOCKTBL
+                           {
+                               FirstName = tbl.First_Name,
+                               LastName = tbl.Last_Name,
+                               Email = tbl.Email,
+                               Id = tbl.Id
+                           };
+                return cust.ToList();
+            }
+        }
+
+        public static UI.MOCKTBL GetEntryByID(int? Id)
+        {
+            var c = new UI.MOCKTBL();
+            using (var db = new MOCKTESTDBEntities())
+            {
+                var cust = db.MOCKTBLs.Where(x => x.Id == Id).FirstOrDefault();
+                if (cust != null)
+                {
+                    c.FirstName = cust.First_Name;
+                    c.LastName = cust.Last_Name;
+                    c.Email = cust.Email;
+                    c.Id = cust.Id;
+                }
+                return c;
+            }
+        }
+
+        public static void SaveRecord(UI.MOCKTBL cust)
+        {
+            using (var db = new MOCKTESTDBEntities())
+            {
+                if (cust.Id > 0)
+                {
+                    var cl = db.MOCKTBLs.Where(x => x.Id == cust.Id).FirstOrDefault();
+                    cl.First_Name = cust.FirstName;
+                    cl.Last_Name = cust.LastName;
+                    cl.Email = cust.Email;
+                }
+                else
+                {
+                    MOCKTBL c = new MOCKTBL();
+                    c.First_Name = cust.FirstName;
+                    c.Last_Name = cust.LastName;
+                    c.Email = cust.Email;
+                    db.MOCKTBLs.Add(c);
+                }
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ TestProj/Controllers/MOCKTBLController.cs
@@ -0,0 +1,59 @@
+﻿using MOCKDAL.Helper;
+using MOCKDAL.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestProj.ViewModel;
+
+namespace TestProj.Controllers
+{
+    public class MOCKTBLController : Controller
+    {
+        // GET: CustomerList 
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<MOCKTBLViewModel> mTbl = Common.GetEntries().Select(x => new MOCKTBLViewModel { FirstName = x.FirstName, LastName = x.LastName, Id = x.Id, Email = x.Email }).ToList();
+            //var custList = Common.GetEntries();
+            ViewBag.MOCKTBLList = mTbl;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(MOCKTBLViewModel cust)
+        {
+            MOCKTBL cl = new MOCKTBL
+            {
+                FirstName = cust.FirstName,
+                LastName = cust.LastName,
+                Email = cust.Email,
+                Id = cust.Id
+            };
+            Common.SaveRecord(cl);
+            return View(cust);
+        }
+
+        [HttpGet]
+        public ActionResult AddNew()
+        {
+            var cust = new MOCKTBLViewModel();
+            return PartialView("Update", cust);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var cust = new MOCKTBLViewModel();
+            if (id != 0)
+            {
+                var c = Common.GetEntryByID(id);
+                cust.FirstName = c.FirstName;
+                cust.LastName = c.LastName;
+                cust.Email = c.Email;
+                cust.Id = c.Id;
+            }
+            return PartialView("Update", cust);
+        }
+    }
+}
